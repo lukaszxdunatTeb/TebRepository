6c318aec0ad0063891be43c5c40e598bb1139e1b
Lukasz Dunat <lukaszxdunat@gmail.com>
2023-11-11 12:04:59 +0100

    If statement example

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int liczba = 5;
+
+        if (liczba > 0) {
+            System.out.println("Liczba jest dodatnia.");
+        } else if (liczba < 0) {
+            System.out.println("Liczba jest ujemna.");
+        } else {
+            System.out.println("Liczba jest równa zero.");
+        }
     }
 }
