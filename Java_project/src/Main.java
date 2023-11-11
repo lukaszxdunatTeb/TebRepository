3ce0d807158d3def718e945ec3694651b51bf8de
Lukasz Dunat <lukaszxdunat@gmail.com>
2023-11-11 12:09:28 +0100

    comparison of numbers - if statement

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int liczba = 7;
+        int liczba1 = 10;
+        int liczba2 = 20;
 
-        if (liczba % 2 == 0) {
-            System.out.println("Liczba jest parzysta.");
+        if (liczba1 > liczba2) {
+            System.out.println("Liczba 1 jest większa od liczby 2.");
+        } else if (liczba1 < liczba2) {
+            System.out.println("Liczba 1 jest mniejsza od liczby 2.");
         } else {
-            System.out.println("Liczba jest nieparzysta.");
+            System.out.println("Liczba 1 jest równa liczbie 2.");
         }
     }
 }
