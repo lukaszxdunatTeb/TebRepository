9f0863091369240698e2b892c7b05e2ecd49dd36
Lukasz Dunat <lukaszxdunat@gmail.com>
2023-11-11 12:20:40 +0100

    Retrieving data from the user until completion is specified - do while loop

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String dane = "";
-
-        while (!dane.equals("koniec")) {
+        
+ /*      W poprzednim commicie jest ten sam program lecz z wykorzystaniem pętli while.
+        Do while w tym przypadku jest lepszym wyborem.*/
+        String dane;
+        do {
             System.out.print("Wprowadź dane (wpisz 'koniec' aby zakończyć): ");
             dane = scanner.nextLine();
             System.out.println("Wprowadziłeś: " + dane);
-        }
+        } while (!dane.equals("koniec"));
 
         // Zawsze zamykaj scanner po użyciu.
         scanner.close();
