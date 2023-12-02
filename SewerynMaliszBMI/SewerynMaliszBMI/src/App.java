8b700be10daf1fd16fb45086a3713de5f160f7cb
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-02 02:57:03 +0100

    Liczenie BMI

--- SewerynMaliszBMI/SewerynMaliszBMI/src/App.java
+++ SewerynMaliszBMI/SewerynMaliszBMI/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Podaj wagę: ");
+
+        double waga = getValue();
+
+        System.out.print("Podaj wzrost: ");
+
+        double wzrost = (getValue())/100;
+
+        double BMI = waga / (wzrost*wzrost);
+        
+        System.out.println(""+BMI);
+
+    }
+
+    public static double getValue() {
+        do {
+            try {
+                Scanner consoleInput = new Scanner(System.in);
+                
+                return consoleInput.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Podano złą wartość. Spróbuj jeszcze raz.");
+            }
+        } while(true);
     }
 }
