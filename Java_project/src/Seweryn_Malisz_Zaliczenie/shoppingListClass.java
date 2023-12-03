793a675051b2634ca2ac5d82c33381b776ab8f32
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-04 00:21:41 +0100

    Deklaracja tablicy elementów listy oraz obiektu przechowującego informacje o nich (nazwa i ilość). SM

--- Java_project/src/Seweryn_Malisz_Zaliczenie/shoppingListClass.java
+++ Java_project/src/Seweryn_Malisz_Zaliczenie/shoppingListClass.java
@@ -5,5 +5,21 @@
 
 package Seweryn_Malisz_Zaliczenie;
 
-public class shoppingListClass {
-}
+    public class shoppingListClass {
+
+        shoppingListElement shoppingListEntry[] = new shoppingListElement[0];
+        int entriesAmount = 0;
+        class shoppingListElement{
+            String EntryValue;
+            int EntryAmount;
+
+            shoppingListElement(String inputName, int inputValue){
+                EntryValue = inputName;
+                EntryAmount = inputValue;
+            }
+        }
+        void addNewElement(String inputName, int inputValue){
+            entriesAmount++;
+            
+        }
+    }
