33832e072fe1b12148f6a7d242d3fee13c16fadc
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-03 22:19:27 +0100

    Initial Commit SM

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -2,35 +2,7 @@ import java.awt.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Witam w liście zakupów!");
-        System.out.println("Aby zakończyć działanie programu napisz 'koniec' ");
-        String[] Tablicazakupów;
-        Tablicazakupów = new String[10];
-        Tablicazakupów[0] = "Mleko";
-        Tablicazakupów[1] = "Chleb";
-        Tablicazakupów[2] = "Masło";             ////tablica produktów
-        Tablicazakupów[3] = "Zgrzewka wody ";
-        Tablicazakupów[4] = "Makaron";
-        Tablicazakupów[5] = "Pomidory";
-        System.out.println("Aktualna Lista zakupów :");
-        for (int i = 0; i < Tablicazakupów.length; i++) {
-            System.out.println(Tablicazakupów[i]);
-        }
-        while (true) {
-            System.out.println("Proszę podaj produkt nowy produkt ?");/////Dodawanie nowycch produktów
-            String produkt = scanner.next();
-
-            if (produkt.equalsIgnoreCase("koniec")) {
-                System.out.println("program zosta zakończony");
-                return;
-            }
-
-
-            System.out.println("czy chcesz usunąć produkt z listy ?");
-
-
-
+        
         }
     }
 }
