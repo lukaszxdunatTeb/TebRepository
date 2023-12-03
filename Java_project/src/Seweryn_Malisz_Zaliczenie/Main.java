216ccdb7cd041abb298615b52dff1e21cbb81e53
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-03 23:59:40 +0100

    Stworzenie klasy do obsługi listy zakupów. SM

--- Java_project/src/Seweryn_Malisz_Zaliczenie/Main.java
+++ Java_project/src/Seweryn_Malisz_Zaliczenie/Main.java
@@ -12,12 +12,16 @@ package Seweryn_Malisz_Zaliczenie;
 
             globalStringVariablesPL text = new globalStringVariablesPL();
 
+            //klasaTypuZmiennejListyZakupów listaZakupów = new klasaTypuZmiennejListyZakupów(); - mniej wiecej coś takiego
+
             boolean programExecution = true;
 
+            System.out.println(text.globalHello);
+
             do{
-                System.out.println(text.globalHello);
 
                 System.out.println(text.mainMenuHeader);
+
                 System.out.println(text.mainMenuEntry[0]);
                 System.out.println(text.mainMenuEntry[1]);
                 System.out.println(text.mainMenuEntry[2]);
@@ -27,11 +31,23 @@ package Seweryn_Malisz_Zaliczenie;
                 System.out.print("> ");
 
                 switch (inputDataClass.get_Value_Int()){
-                    case 1:
+                    case 1:                 //Dodaj nowy wpis
 
                         break;
-                        
-                    default:
+
+                    case 2:                 //Usuń wpis
+
+                        break;
+
+                    case 3:                 //Wyświetl wszystkie wpisy
+
+                        break;
+
+                    case 4:                 //Wyszukaj i wyświetl wpis wpis
+
+                        break;
+
+                    default:                 //Zakończ program
                         programExecution = false;
                         break;
                 }
---
+++ Java_project/src/Seweryn_Malisz_Zaliczenie/shoppingListClass.java
@@ -0,0 +1,9 @@
+/*
+*   Klasa przechowująca zmienne i funkcje związane z samą listą zakupów
+*
+*/
+
+package Seweryn_Malisz_Zaliczenie;
+
+public class shoppingListClass {
+}
