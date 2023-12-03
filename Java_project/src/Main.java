91383061b58153db9285aab28cb49b691b94fe82
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-03 22:33:22 +0100

    Udzielenie odpowiedzi na pytania z zaliczenia SM

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,12 +1,6 @@
-import java.awt.*;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
         }
     }
-}
-
-    ////// git commit-m "opis zmian" = To kolejny commit który posiada nowa wersję  kodu który pisaliśmy .
-    ///// git clone<adress repo> =  używamy go gdy chcemy pobrać projekt/zadanie ze zdalnego repozytorium które eznajduję się np na githubie.
-    /////   git checkout <nazwa_gałęzi> = używamy ją gdy chcemy przejśc na nowy/inny brunch
