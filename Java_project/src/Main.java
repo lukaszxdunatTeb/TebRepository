e9a9ae69ef3a5cf092029f657e740c7d8c16cc44
Lukasz Dunat <lukasz.dunat@capgemini.com>
2023-12-10 12:04:59 +0100

    Inicjalizacja obiektow dziedziczacych klase Monster

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,17 +1,17 @@
-import Interface.Dragon;
-import Interface.Skieleton;
+import Abstrakcja.Dragon;
+import Abstrakcja.Skieleton;
 
 import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        Skieleton skieleton = new Skieleton("Sword"); // Stworzenie szkieleta za pomoca konstruktora parametrycznego
-        skieleton.attack();
-        skieleton.displayDescription();
+        Skieleton skieleton = new Skieleton(100, "Szkielet nr. 1", "Sword"); // stworzenie szkieleta za pomoca konstrutkora parametrycznego
+        skieleton.setHP(10);
+        skieleton.move();
+        out.println(skieleton.getWeapon());
 
-        Dragon dragon = new Dragon(50); // Stworzenie smoka za pomoca konstruktora parametrycznego
-        out.println(dragon.getAge()); // pobranie wieku za pomoca gettera
-        dragon.setName("Władek");  // ustawienie imienia za pomoca settera
-        dragon.displayDescription();
+        Dragon dragon = new Dragon(50, "Smok nr.1");
+        out.println(dragon.getName());
+        dragon.move();
     }
 }
