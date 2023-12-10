84ce3ee7087c42ff25544f46fa9c6fa669fbf12d
Lukasz Dunat <lukasz.dunat@capgemini.com>
2023-12-10 12:00:30 +0100

    Inicjalizacja obiektow implementujacych interfejs

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,7 +1,17 @@
-import java.util.Scanner;
+import Interface.Dragon;
+import Interface.Skieleton;
+
+import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
+        Skieleton skieleton = new Skieleton("Sword"); // Stworzenie szkieleta za pomoca konstruktora parametrycznego
+        skieleton.attack();
+        skieleton.displayDescription();
 
+        Dragon dragon = new Dragon(50); // Stworzenie smoka za pomoca konstruktora parametrycznego
+        out.println(dragon.getAge()); // pobranie wieku za pomoca gettera
+        dragon.setName("Władek");  // ustawienie imienia za pomoca settera
+        dragon.displayDescription();
     }
 }
