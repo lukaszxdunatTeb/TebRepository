2fad6acf821fd5fc570e63f52393031c31770af6
lazarowskii11 <lazarowskii20>
2023-12-11 17:54:01 +0100

    Dodanie możliwości  dodania produktu oraz sprawdzenie czy produkt jest na liście zakupów

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,70 +1,84 @@
+
+import java.util.Arrays;
 import java.util.Scanner;
-import javax.swing.*:
-import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /// zmienna do pętli while
+        boolean działanieprogramu = true;
 
-        public class Main {
-            public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                /// zmienna do pętli while
-                boolean działanieprogramu = true;
-                String[] lista = new String[5];                        // DEKLARACJA TABLICY
-                while (działanieprogramu) {
-                    System.out.println("### witam w liście zakupów ###");
-                    System.out.println("Wybierz opcję która cię interesuje. ");
-                    System.out.println("1.Dodawanie produktów do listy. ");
-                    System.out.println("2.Usuwanie produktów z listy.   ");                    /// menu
-                    System.out.println("3.Wyświetlanie listy zakupów.   ");
-                    System.out.println("4.Sprawdzanie czy produkt jest na liście.  ");
-                    System.out.println("5.Zakończenie działania programu:    ");
-                    int wybór = scanner.nextInt();
-                    switch (wybór) {
+        while (działanieprogramu) {
+            System.out.println("### witam w liście zakupów ###");
+            System.out.println("Wybierz opcję która cię interesuje. ");
+            System.out.println("1.Dodawanie produktów do listy. ");
+            System.out.println("2.Usuwanie produktów z listy.   ");                    /// menu
+            System.out.println("3.Wyświetlanie listy zakupów.   ");
+            System.out.println("4.Sprawdzanie czy produkt jest na liście.  ");
+            System.out.println("5.Zakończenie działania programu:    ");
+            int wybór = scanner.nextInt();
+            String[]lista =new String[3];
+            switch (wybór) {
 
-                        case 1:                                                      /// Dodawanie produktu
-                            System.out.println("Nazwa produktu który chcesz dodać :");
-                            String nowyprodukt;
-                            nowyprodukt= scanner.next();
-
-                            break;
-
-                        case 2:                                                     ////Usuwanie produktów
-
-
-
-
-                            break;
-
-                        case 3:                                                     ///// wyświetlanie
-                            for(int i = 0; i < lista.length; i++ ) {
-                                System.out.println(lista[i]);
-
-                            }
-                            break;
-                        case 4 :                                                    ///// Sprawdzanie
-
-
-
-
-                            break;
-                        case 5:
-                            System.out.println(" Dziekuję za skorzystanie z  listy zakupów");  //// zakońćzenie
-                            return;
-
-
-                        default:
-                            System.out.println("Nie wybrałeś opcji ,prosze wybierz opcję ponownie !");
-                            break;
-
-
-
-
-
-
+                case 1: {
+                    //// dodawaniue produktu
+                    System.out.println("Nazwa produktów które już są na liście :");
+                    System.out.println(Arrays.toString(lista));
+                    int length_Var = lista.length;
+                    System.out.println("Nazwa produktu który chcesz dodać :");
+                    String nowyprodukt ;
+                    nowyprodukt= scanner.next();
+                    String[] listaz = new String[length_Var + 1];
+                    for (int i = 0; i < lista.length; i++) {
+                        listaz[i] = lista[i];
                     }
+                    listaz[listaz.length - 1] = nowyprodukt;
+                    lista=listaz;
+
+                    break;
                 }
+
+                case 2:                                                     ////Usuwanie produktów
+
+
+
+
+                   break;
+
+                case 3:                                                     ///// wyświetlanie
+
+                    break;
+
+                case 4 :                                                    //// sprawdzenie
+                    System.out.println("Nazwa produktu do sprawdzenia :");
+                    for (int i=0; i < lista.length-1; i++){
+                        String produktdospr;
+                        produktdospr=scanner.nextLine();
+                        if (lista[i].equals(produktdospr)){
+                            System.out.println("Produkt znajduje się na liście.");
+                            return;
+                        }
+                    }
+                    System.out.println("Produktu nie ma na liście.");
+
+
+
+                    break;
+                case 5:
+                    System.out.println(" Dziekuję za skorzystanie z  listy zakupów");
+                    return;
+
+
+                default:
+                    System.out.println("Nie wybrałeś opcji ,prosze wybierz opcję ponownie !");
+                    break;
+
+
+
+
 
             }
         }
 
-
-
+    }
+}
