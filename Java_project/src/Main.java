e640c1c481e830b780ba1b7bf9a79982366f3832
Dmitriy Vygovskiy <programista.art@gmail.com>
2023-12-16 15:08:24 +0100

    Pliki Konwerter Temperatur

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,59 +1,44 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        //Konwerter Temperatury
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj pierwszą liczbę: ");
-        double number1 = scanner.nextDouble();
+        while (true) {
+            System.out.println("Wybierz rodzaj konwersji:");
+            System.out.println("1. Celsiusz --> Fahrenheit");
+            System.out.println("2. Fahrenheit --> Celsiusz");
+            System.out.println("Wpisz 'koniec' aby zakończyć program.");
 
-        System.out.println("Podaj drugą liczbę");
-        double number2 = scanner.nextDouble();
+            String chooseAction = scanner.nextLine();
 
-        System.out.println("Wybierz operacje (1 - dodawanie, 2 - odejmowanie, 3 - mnożenie, 4 - dzielenie): ");
-        int operation = scanner.nextInt();
+            if (chooseAction.equalsIgnoreCase("koniec")) {
+                System.out.println("Koniec programu.");
+                break;
+            }
 
-        double result = 0;
+            if (chooseAction.equals("1")) {
+                System.out.println("Podaj temperaturę w stopniach Celsiusza:");
+                double celsius = scanner.nextDouble();
+                double fahrenheit = konvertCelsiusToFahrenheit(celsius);
+                System.out.println("Wynik: " + fahrenheit + " stopni Fahrenheit");
+            } else if (chooseAction.equals("2")) {
+                System.out.println("Podaj temperaturę w stopniach Fahrenheita:");
+                double fahrenheit = scanner.nextDouble();
+                double celsius = konvertFahrenheitToCelsius(fahrenheit);
+                System.out.printf("Wynik: " + celsius + " stopni Celsiusza");
+            } else {
+                System.out.println("Nieprawidłowy wybór. Wybierz ponownie.");
+            }
 
-        switch (operation) {
-            case 1:
-                result = addition(number1, number2);
-                break;
-            case 2:
-                result = subtraction(number1, number2);
-                break;
-            case 3:
-                result = multiplication(number1, number2);
-                break;
-            case 4:
-                result = division(number1, number2);
-                break;
-            default:
-                System.out.println("Nieprawidłowy wybór operacji.");
-                break;
+            scanner.nextLine();
         }
-
-        System.out.println("Wynik: " + result);
+    }
+    private static double konvertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 
-    private static double addition(double a, double b) {
-        return a + b;
-    }
-
-    private static double subtraction(double a, double b) {
-        return a - b;
-    }
-
-    private static double multiplication(double a, double b) {
-        return a * b;
-    }
-
-    private static double division(double a, double b) {
-        if (b != 0) {
-            return a / b;
-        } else {
-            System.out.println("Nie można dzielić przez zero.");
-            return 0;
-        }
+    private static double konvertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
