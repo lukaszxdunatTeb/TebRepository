6f2726648245ccd9673238f46111515c7558e7fe
Seweryn Malisz <malisz.seweryn@gmail.com>
2023-12-26 19:28:07 +0100

    Przetworzenie stałych tekstowych. Dodatnie możliwości wyboru języka. Dodanie możliwości wyświetlania menu głównego programu

--- Java_project/src/Main.java
+++ Java_project/src/Main.java
@@ -1,3 +1,7 @@
+/*
+
+ */
+
 import InputData.InputDataClass;
 
 import java.util.Locale;
@@ -8,6 +12,27 @@ public class Main {
 
     static InputDataClass input = new InputDataClass();
     public static void main(String[] args) {
+
+        //Wybór języka
+        selectLanguage();
+
+        //Powitanie
+        System.out.println(resourceBundle.getString("global_hello"));
+
+        //Wypisanie menu głównego
+
+        //Zakończenie programu - pożegnanie
+        System.out.println(resourceBundle.getString("global_goodbye"));
+    }
+
+    //Funkcja ustawienia języka programu
+    private static void setLocale(String language) {
+        Locale locale = new Locale(language);
+        resourceBundle = ResourceBundle.getBundle("LanguagePack", locale);
+    }
+
+    //Funkcja wyboru języka programu
+    private static void selectLanguage(){
         boolean languageNotSelected = true;
         do {
             System.out.println("1 - Polski");
@@ -23,14 +48,16 @@ public class Main {
                     break;
                 default:
             }
-        }while (languageNotSelected);
-
-        System.out.println(resourceBundle.getString("global_hello"));
-
+        } while (languageNotSelected);
     }
 
-    private static void setLocale(String language) {
-        Locale locale = new Locale(language);
-        resourceBundle = ResourceBundle.getBundle("LanguagePack", locale);
+    private static void showMainMenu(){
+        //Nagłówek menu -
+        System.out.println(resourceBundle.getString("main_menu_header"));
+        System.out.println(resourceBundle.getString("main_menu_entry_add_new_item"));
+        System.out.println(resourceBundle.getString("main_menu_entry_remove_item_from_list"));
+        System.out.println(resourceBundle.getString("main_menu_entry_show_current_list"));
+        System.out.println(resourceBundle.getString("main_menu_entry_check_item"));
+        System.out.println(resourceBundle.getString("main_menu_entry_close_program"));
     }
 }
